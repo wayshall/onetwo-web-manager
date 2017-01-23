401e2facc6e761f287c1e075fa3db069b832850a
wayshall <weishao.zeng@gmail.com>
2017-01-23 15:39:45 +0800

    add oauth2 config

--- src/main/java/org/onetwo/webapp/manager/AppContextConfig.java
+++ src/main/java/org/onetwo/webapp/manager/AppContextConfig.java
@@ -1,13 +1,17 @@
 package org.onetwo.webapp.manager;
 
+import org.onetwo.boot.module.security.oauth2.EnableUserInfoResource;
 import org.onetwo.boot.module.security.url.EnableUrlSecurity;
 import org.onetwo.plugins.admin.utils.WebAdminPermissionConfig.RootMenuClassProvider;
 import org.onetwo.webapp.manager.utils.Systems;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @Configuration
 @EnableUrlSecurity
+@EnableAuthorizationServer
+@EnableUserInfoResource
 public class AppContextConfig  {
 
 	@Bean
