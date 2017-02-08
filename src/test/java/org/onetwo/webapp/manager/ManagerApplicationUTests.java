a112d72469f9443a91ab88234add494dccadbd64
wayshall <weishao.zeng@gmail.com>
2017-02-08 11:27:15 +0800

    fix pom;

--- src/test/java/org/onetwo/webapp/manager/ManagerApplicationUTests.java
+++ src/test/java/org/onetwo/webapp/manager/ManagerApplicationUTests.java
@@ -6,9 +6,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 //@SpringApplicationConfiguration(classes = WebManagerStarter.class)
 @SpringBootTest(classes=WebManagerStarter.class)
 public class ManagerApplicationUTests {
