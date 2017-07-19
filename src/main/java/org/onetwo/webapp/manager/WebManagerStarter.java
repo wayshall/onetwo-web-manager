4d33710f12499bedf4abf4f2c7ad431600432b3c
wayshall <weishao.zeng@gmail.com>
2017-07-19 23:59:29 +0800

    bugfix

--- src/main/java/org/onetwo/webapp/manager/WebManagerStarter.java
+++ src/main/java/org/onetwo/webapp/manager/WebManagerStarter.java
@@ -6,8 +6,6 @@ import org.onetwo.dbm.spring.EnableDbm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.groovy.template.GroovyTemplateAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,17 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 //@SpringBootApplication(exclude={VelocityAutoConfiguration.class})
 @EnableJFishBootExtension(appcationType=AppcationType.WEB_UI)
 @EnableDbm
-public class WebManagerStarter extends SpringBootServletInitializer {
+public class WebManagerStarter {
 	
 	@RequestMapping("/")
     String home() {
         return "index";
     }
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(WebManagerStarter.class);
-	}
 	
     public static void main(String[] args) {
         SpringApplication.run(WebManagerStarter.class, args);
